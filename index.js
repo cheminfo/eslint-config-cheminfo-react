8abd98d218e2dddae55e4de89df5d42b6a494501
Michaël Zasso <targos@protonmail.com>
2018-06-26 09:13:52 +0200

    feat: upgrade to ESLint 5 and switch ecmaVersion to 2018

--- index.js
+++ index.js
@@ -1,59 +1,55 @@
 'use strict';
 
 module.exports = {
-    env: {
-        es6: true,
-        browser: true
-    },
-    parserOptions: {
-        ecmaVersion: '2017',
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-            experimentalObjectRestSpread: true
-        }
-    },
-    extends: 'eslint-config-cheminfo',
-    plugins: [
-        'react'
-    ],
-    rules: {
-        'indent': [2, 2, {SwitchCase: 1}],
-        'jsx-quotes': ['error', 'prefer-double'],
-        'import/no-unassigned-import': ['warn', { allow: ['**/*.css'] }],
-        'react/jsx-closing-bracket-location': 2,
-        'react/jsx-closing-tag-location': 2,
-        'react/jsx-curly-spacing': 2,
-        'react/jsx-equals-spacing': 2,
-        'react/jsx-indent': [2, 2],
-        'react/jsx-indent-props': [2, 2],
-        'react/jsx-key': 2,
-        'react/jsx-no-bind': 1,
-        'react/jsx-no-undef': 2,
-        'react/jsx-wrap-multilines': 2,
-        'react/jsx-tag-spacing': 2,
-        'react/jsx-uses-react': 2,
-        'react/jsx-uses-vars': 2,
-        'react/jsx-max-props-per-line': [2, { when: 'multiline' }],
-        'react/button-has-type': 2,
-        'react/no-access-state-in-setstate': 1,
-        'react/no-array-index-key': 1,
-        'react/no-danger': 1,
-        'react/no-danger-with-children': 1,
-        'react/no-did-update-set-state': 2,
-        'react/no-direct-mutation-state': 2,
-        'react/no-multi-comp': 2,
-        'react/no-redundant-should-component-update': 2,
-        'react/no-render-return-value': 2,
-        'react/no-typos': 2,
-        'react/no-unknown-property': 2,
-        'react/no-unused-state': 2,
-        'react/no-will-update-set-state': 2,
-        'react/prefer-stateless-function': [2, {ignorePureComponents: true}],
-        'react/require-render-return': 2,
-        'react/self-closing-comp': 2,
-        'react/sort-comp': 2,
-        'react/style-prop-object': 2,
-        'react/void-dom-elements-no-children': 2
+  env: {
+    es6: true,
+    browser: true
+  },
+  parserOptions: {
+    ecmaVersion: '2018',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
     }
+  },
+  extends: 'eslint-config-cheminfo',
+  plugins: ['react'],
+  rules: {
+    'jsx-quotes': ['error', 'prefer-double'],
+    'import/no-unassigned-import': ['warn', { allow: ['**/*.css'] }],
+    'react/jsx-closing-bracket-location': 2,
+    'react/jsx-closing-tag-location': 2,
+    'react/jsx-curly-spacing': 2,
+    'react/jsx-equals-spacing': 2,
+    'react/jsx-indent': [2, 2],
+    'react/jsx-indent-props': [2, 2],
+    'react/jsx-key': 2,
+    'react/jsx-no-bind': 1,
+    'react/jsx-no-undef': 2,
+    'react/jsx-wrap-multilines': 2,
+    'react/jsx-tag-spacing': 2,
+    'react/jsx-uses-react': 2,
+    'react/jsx-uses-vars': 2,
+    'react/jsx-max-props-per-line': [2, { when: 'multiline' }],
+    'react/button-has-type': 2,
+    'react/no-access-state-in-setstate': 1,
+    'react/no-array-index-key': 1,
+    'react/no-danger': 1,
+    'react/no-danger-with-children': 1,
+    'react/no-did-update-set-state': 2,
+    'react/no-direct-mutation-state': 2,
+    'react/no-multi-comp': 2,
+    'react/no-redundant-should-component-update': 2,
+    'react/no-render-return-value': 2,
+    'react/no-typos': 2,
+    'react/no-unknown-property': 2,
+    'react/no-unused-state': 2,
+    'react/no-will-update-set-state': 2,
+    'react/prefer-stateless-function': [2, { ignorePureComponents: true }],
+    'react/require-render-return': 2,
+    'react/self-closing-comp': 2,
+    'react/sort-comp': 2,
+    'react/style-prop-object': 2,
+    'react/void-dom-elements-no-children': 2
+  }
 };
