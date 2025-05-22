d3c6562755852f6256672438397346dc64a89e43
Michaël Zasso <targos@protonmail.com>
2025-05-22 15:47:23 +0100

    fix: React should not be in scope (#48)

--- test/test.js
+++ test/test.js
@@ -6,6 +6,39 @@ const ESLint = await loadESLint({ useFlatConfig: true });
 /** @type {import('eslint').ESLint} */
 const eslint = new ESLint();
 
-const [okResult] = await eslint.lintFiles(['test/ok.jsx']);
+const [okResult, notOkResult] = await eslint.lintFiles([
+  'test/ok.jsx',
+  'test/not_ok.jsx',
+]);
 
 assert.strictEqual(okResult.errorCount, 0, 'ok.jsx should have no error');
+
+const errors = notOkResult.messages.filter(isError).map(getRuleId).sort();
+
+assert.deepStrictEqual(errors, [
+  'no-unused-vars',
+  'react-hooks/exhaustive-deps',
+]);
+
+const warnings = notOkResult.messages
+  .filter(isWarning)
+  .filter(excludeJsdoc)
+  .map(getRuleId)
+  .sort();
+assert.deepStrictEqual(warnings, []);
+
+function isError(message) {
+  return message.severity === 2;
+}
+
+function isWarning(message) {
+  return message.severity === 1;
+}
+
+function excludeJsdoc(message) {
+  return !message.ruleId.startsWith('jsdoc/');
+}
+
+function getRuleId(message) {
+  return message.ruleId;
+}
