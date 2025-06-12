e56b9f82466fa39e52bab2315b53774ffd3744d1
Michaël Zasso <targos@protonmail.com>
2025-06-12 09:56:37 +0200

    feat: add `react-hooks/react-compiler` rule (#53)

--- test/not_ok.jsx
+++ test/not_ok.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
-export default function MyComponent() {
+export default function MyComponent(props) {
+  props.x = 42; // Mutating props is not allowed by React compiler.
   const [value] = useState('');
   useEffect(() => value, []);
   return <div>Hello world</div>;
