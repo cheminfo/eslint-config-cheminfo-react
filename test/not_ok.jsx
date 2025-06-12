627eb49e3934ebdcca2c3bd55f49650015b990ee
Daniel Kostro <kostro.d@gmail.com>
2025-06-12 10:39:11 +0200

    chore: update you might not need an effect test use node testing library

--- test/not_ok.jsx
+++ test/not_ok.jsx
@@ -3,5 +3,6 @@ import React, { useEffect, useState } from 'react';
 export default function MyComponent() {
   const [value] = useState('');
   useEffect(() => value, []);
+  // useEffect(() => {}, []);
   return <div>Hello world</div>;
 }
