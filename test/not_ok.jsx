fcfffb47d64346f3a4e6ff6045bac984c2ee9e42
Michaël Zasso <targos@protonmail.com>
2025-06-12 10:06:15 +0200

    fix: allow referrer in `target="_blank"` links
    Closes: https://github.com/cheminfo/eslint-config-cheminfo-react/issues/45

--- test/not_ok.jsx
+++ test/not_ok.jsx
@@ -4,5 +4,11 @@ export default function MyComponent(props) {
   props.x = 42; // Mutating props is not allowed by React compiler.
   const [value] = useState('');
   useEffect(() => value, []);
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <a href="https://example.com/" target="_blank">
+        External link
+      </a>
+    </div>
+  );
 }
